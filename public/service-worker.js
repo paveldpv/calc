c03716310e7093b54fd9e2bac0406020425f4dc7
pavel <paveldpv91@gmail.com>
2022-09-24 12:50:31 +0300

    media

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,34 +1,34 @@
-const staticCacheName = `s-app-v2`
+const staticCacheName = `s-app-v3`
 
-const assetURLs=[
+const assetURLs = [
    'index.html',
    `static/js/bundle.js`
 ]
 
-self.addEventListener(`install`,async e=>{
+self.addEventListener(`install`, async e => {
    const cache = await caches.open(staticCacheName)
    await cache.addAll(assetURLs)
 
 })
 
-self.addEventListener(`activate`,async e=>{
+self.addEventListener(`activate`, async e => {
    const chahesNames = await caches.keys()
    await Promise.all(
-      chahesNames.filter(name =>name!=staticCacheName)
-      .map(name=>caches.delete(name))//очищаем старые кеш
+      chahesNames.filter(name => name != staticCacheName)
+         .map(name => caches.delete(name))//очищаем старые кеш
    )
-   
+
 })
 
-self.addEventListener(`fetch`,e=>{
-   console.log(`fethc`,e.request);
+self.addEventListener(`fetch`, e => {
+   console.log(`fethc`, e.request);
 
    e.respondWith(cahceFirst(e.request))
 })
 
 
 
-async function cahceFirst(request){
-  const cahced =  await caches.match(request)
-  return cahced ?? await fetch(request)
+async function cahceFirst(request) {
+   const cahced = await caches.match(request)
+   return cahced ?? await fetch(request)
 }
